f7b50bb37b0b3c558e22935d156fcdbde15e7add
Rajesh Budhiraja <rajesh.budhiraja@zomato.com>
2025-04-19 10:04:12 +0530

    NukeExtensions is added

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -25,6 +25,7 @@ let package = Package(
                 .product(name: "JTAppleCalendar", package: "JTAppleCalendar"),
                 .product(name: "Lottie", package: "lottie-spm"),
                 .product(name: "Nuke", package: "Nuke"),
+                .product(name: "NukeExtensions", package: "Nuke"),
                 .product(name: "ZMarkupParser", package: "ZMarkupParser"),
                 .product(name: "Alamofire", package: "Alamofire")
             ]),
