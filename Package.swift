b4c1fc336b121682c4ce6b06fc4cd0b21d293992
Rajesh Budhiraja <rajesh.budhiraja@zomato.com>
2025-04-24 10:48:35 +0530

    Revert alamofire version bump

--- Package.swift
+++ Package.swift
@@ -17,18 +17,17 @@ let package = Package(
         .package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.5.1")),
         .package(url: "https://github.com/kean/Nuke.git", .upToNextMajor(from: "12.8.0")),
         .package(url: "https://github.com/BudhirajaRajesh/ZMarkupParser.git", .exact("2.0.0")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.4")),
     ],
     targets: [
         .target(
             name: "NuggetExternalDependency",
             dependencies: [
                 .product(name: "JTAppleCalendar", package: "JTAppleCalendar"),
-                .product(name: "Lottie", package: "lottie-spm"),
                 .product(name: "Nuke", package: "Nuke"),
                 .product(name: "NukeExtensions", package: "Nuke"),
                 .product(name: "ZMarkupParser", package: "ZMarkupParser"),
-                .product(name: "AlamofireDynamic", package: "Alamofire")
+                .product(name: "Alamofire", package: "Alamofire")
             ]),
         .testTarget(
             name: "NuggetExternalDependencyTests",
